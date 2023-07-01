ea63b1ae674a48cc987515ea01b25f6d7af3bd23
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-07-01 20:57:55 +0200

    Add files via upload

---
+++ lectureSix/lectureSix/HomeViewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewViewController.swift
+//  lectureSix
+//
+//  Created by Svilen Petrov on 30/01/2023.
+//
+
+import UIKit
+
+class HomeViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
